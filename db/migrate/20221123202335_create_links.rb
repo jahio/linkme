d9b45fca6d51d5d15ebc73d8fa70ae5e5772c2a9
J. Austin Hughey <git@jah.io>
2022-11-23 15:33:00 -0600

    database schema

--- db/migrate/20221123202335_create_links.rb
+++ db/migrate/20221123202335_create_links.rb
@@ -1,8 +1,36 @@
 class CreateLinks < ActiveRecord::Migration[7.0]
   def change
     create_table :links, id: :uuid do |t|
-
+      t.text            :url
+      t.bigint          :timestamp
+      t.text            :token
+      t.bigint          :visits
+      t.datetime        :last_visit
+      t.text            :creator_ip
+      # Thought about native data types in PG here but that could have unintended side
+      # effects going forward (e.g. automagic/unexpected IP addr datatype/class conversion, etc.)
+      # Best not to over-engineer it, leave it as a string and convert in future as needed.
       t.timestamps
     end
+
+    # We need the combination of URL, timestamp, and token to be unique,
+    # and that name can get a bit too long potentially, so...
+    add_index :links, [:url, :timestamp, :token], unique: true, name: 'links_are_unique'
+
+    # Create additional indices for faster lookup by singular granularity
+    # TBH I'm not 100% certain if these are necessary for all databases or not with the above
+    # index in place, but doing a lookup without ALL of the above columns in a query may
+    # trigger a full table scan without each of these in the query otherwise, so to err on
+    # the side of caution, here we go. Would prefer to discuss this with a DBA under ideal
+    # circumstances.
+    add_index :links, :url
+    add_index :links, :timestamp
+    add_index :links, :token
+
+    # These may not be strictly necessary but future reporting will definitely need this
+    # so we might as well get it out of the way.
+    add_index :links, :last_visit
+    add_index :links, :visits
+    add_index :links, :creator_ip
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_201104) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_202335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "links", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.text "url"
+    t.bigint "timestamp"
+    t.text "token"
+    t.bigint "visits"
+    t.datetime "last_visit"
+    t.text "creator_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_ip"], name: "index_links_on_creator_ip"
+    t.index ["last_visit"], name: "index_links_on_last_visit"
+    t.index ["timestamp"], name: "index_links_on_timestamp"
+    t.index ["token"], name: "index_links_on_token"
+    t.index ["url", "timestamp", "token"], name: "links_are_unique", unique: true
+    t.index ["url"], name: "index_links_on_url"
+    t.index ["visits"], name: "index_links_on_visits"
+  end
 
 end
