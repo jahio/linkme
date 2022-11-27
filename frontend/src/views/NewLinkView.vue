eda2f9c3e128da09436b0368adf36abfe092b9d7
J. Austin Hughey <git@jah.io>
2022-11-27 15:19:16 -0600

    [WIP] frontend application flow

---
+++ frontend/src/views/NewLinkView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="NewLink">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <NewLink />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NewLink from '@/components/NewLink.vue'
+
+export default {
+  name: 'NewLinkView',
+  components: {
+    NewLink
+  }
+}
+</script>
---
+++ frontend/src/views/RedirectView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="RedirectView">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <ExistingLink />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ExistingLink from '@/components/ExistingLink.vue'
+
+export default {
+  name: 'RedirectView',
+  components: {
+    ExistingLink
+  }
+}
+</script>
