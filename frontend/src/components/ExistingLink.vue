fc7cf79518e2211cbc2730cc25b6e82cf9c27182
J. Austin Hughey <git@jah.io>
2022-11-27 21:01:19 -0600

    Needs refinement but works - enter URL -> lookup -> redirect

--- frontend/src/components/ExistingLink.vue
+++ frontend/src/components/ExistingLink.vue
@@ -1,13 +1,51 @@
 <template>
   <div class="ExistingLink">
-    Existing Link View goes here
+    <p>{{short_link}} corresponds to...</p>
+    <h2>
+      <a :href="original_url">{{ original_url }}</a>
+    </h2>
+
+    <p>
+      Redirecting you momentarily...
+    </p>
+
+    <p class="small">
+      ...or just click the link if you're impatient and certain the domain is safe to visit.
+    </p>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'ExistingLink'
-}
+  import LinkService from '@/services/LinkService.js'
+  export default {
+    name: 'ExistingLink',
+    data() {
+      return {
+        original_url: null,
+        short_link: null,
+        shortpath: null,
+        error: null
+      }
+    },
+    created() {
+      LinkService.get(this.$route.params.shortpath).then(response => {
+        console.log('events:', response.data)
+        this.original_url = response.data.original_url
+        this.short_link = response.data.short_link
+        this.shortpath = response.data.shortpath
+        // Set a timeout and then when finished, redirect to the location.
+        // Five seconds.
+        setTimeout(()=> {
+          window.location.replace(this.original_url)
+        }, 5000)
+      })
+      .catch(error => {
+        console.log(error)
+        this.error = error
+      })
+    }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
