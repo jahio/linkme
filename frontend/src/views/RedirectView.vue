231a45ad8ae46355e9538fb698bd9d03e7ad3953
J. Austin Hughey <git@jah.io>
2022-11-27 18:36:12 -0600

    [WIP] mild progress with frontend

--- frontend/src/views/RedirectView.vue
+++ frontend/src/views/RedirectView.vue
@@ -8,11 +8,27 @@
 <script>
 // @ is an alias to /src
 import ExistingLink from '@/components/ExistingLink.vue'
+import axios from 'axios'
 
 export default {
   name: 'RedirectView',
   components: {
     ExistingLink
+  },
+  data() {
+    return {
+      link: null
+    }
+  },
+  created() {
+    axios.defaults.headers.common['Content-Type'] = 'application/json'
+    axios.defaults.headers.common['Accept'] = 'application/json'
+    axios.get("http://localhost:3000/links/SHORTCODE").then(response => {
+      console.log('events:', response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
   }
 }
 </script>
